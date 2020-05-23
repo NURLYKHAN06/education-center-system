b68a1aa90db0b9560406ac2f3fb3f28e4c9d514e
nurlykhan <n.salamatoff@gmail.com>
2020-05-24 02:06:48 +0500

    a

--- .next/static/pGGLOCuqoKvtlzHtHjnSs/pages/login.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{pqMH:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/login",function(){return e("yfTL")}])},yfTL:function(n,t,e){"use strict";e.r(t);var o=e("q1tI"),u=e.n(o),i=(e("nOHt"),u.a.createElement);t.default=function(){return Object(o.useEffect)((function(){}),[]),i(u.a.Fragment,null,"Login")}}},[["pqMH",0,1,2]]]);
